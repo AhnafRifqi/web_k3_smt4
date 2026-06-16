5d4c9dcf84a98e5880b75446d58624cf8b5cc9bf
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2026-06-16 14:39:52 +0700

    mengubah tampilan edit user

--- K3-SMT4/resources/views/users/index.blade.php
+++ K3-SMT4/resources/views/users/index.blade.php
@@ -158,6 +158,7 @@
                                 </button>
                             </form>
 
+                            @if($user->role !== 'admin')
                             <form action="{{ route('users.destroy', $user) }}" method="POST" class="inline-block" onsubmit="return confirm('Hapus pengguna {{ $user->name }}?')">
                                 @csrf
                                 @method('DELETE')
@@ -165,6 +166,7 @@
                                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/></svg>
                                 </button>
                             </form>
+                            @endif
                             @endif
                         </div>
                     </td>
