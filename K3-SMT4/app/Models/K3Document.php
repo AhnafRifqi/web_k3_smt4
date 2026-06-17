ee6670930c10b9c2c54ca477665dcc79a4aade6a
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2026-06-17 14:57:14 +0700

    hard delete

--- K3-SMT4/app/Models/K3Document.php
+++ K3-SMT4/app/Models/K3Document.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class K3Document extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $table = 'k3_documents';
 
